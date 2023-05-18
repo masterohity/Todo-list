c6f1c505420e30271a681999e4449a8b1a2299e7
Rohit Yadav <rohitshivdhaniyadav@gmail.com>
2023-05-18 17:34:15 +0530

    Env require change

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
-const dotenv = require("dotenv").config();
+
 
 const app = express();
 
